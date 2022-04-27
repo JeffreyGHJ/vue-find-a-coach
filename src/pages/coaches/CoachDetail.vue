b5efdf5c578a64dd2cdb6f338bf32ac62acde56e
Jhern703 <50380191+Jhern703@users.noreply.github.com>
2022-04-27 16:20:42 -0400

    First commit - adding project files

---
+++ src/pages/coaches/CoachDetail.vue
@@ -0,0 +1,6 @@
+<template>
+    DETAILS FOR COACH
+    <!-- The CoachDetail route has a child route in it so it needs a routerview element to show it -->
+    <router-view></router-view>
+    <router-link to="/coaches/c1/contact">Contact</router-link>
+</template>
