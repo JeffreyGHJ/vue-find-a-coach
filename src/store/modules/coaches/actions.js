f73e320f95f6d55b688f323723975e3e78d05e59
Jhern703 <JeffreyGHJ@gmail.com>
2022-04-29 15:50:13 -0400

    Connected Firebase backend to persist data
    -modified store/modules/coaches/actions.js
    -registerCoach() changed to async
    -await fetch() used for PUTing a new coach in the db
    -To-Do: actually populate app with fetched data instead of local data
    -To-Do: response error handling

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,8 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId;
         const coachData = {
-            id: context.rootGetters.userId,
+            // id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
@@ -9,6 +10,22 @@ export default {
             areas: data.areas
         };
 
-        context.commit('registerCoach', coachData);
+        // Firebase requires '.json' at the end
+        const response = await fetch(`https://vue-find-a-coach-96fc8-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',      // configure fetch: default = GET --> PUT = create or overwrite
+            body: JSON.stringify(coachData)
+        });          // .then(); --> async await is better
+
+        //const responseData = await response.json();
+        
+        if (!response.ok) {
+            // ERROR HANDLING
+        }
+
+        //context.commit('registerCoach', coachData);
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
     }
 };
