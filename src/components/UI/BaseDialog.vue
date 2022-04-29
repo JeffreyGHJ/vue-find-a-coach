cd7ea2ad57d25ee60fd5fe279d7d354b7c60e6cf
Jhern703 <JeffreyGHJ@gmail.com>
2022-04-29 19:24:15 -0400

    Added dialog and router transition animations
    -added css transition animations to corresponding components
    -Top level App.vue router-view has a transition tag inside
    -child component templates now have one parent element for transition
    -templated NotFound.vue and included a router link back to main page

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,21 +1,23 @@
 <template>
   <teleport to="body">
     <div v-if="show" @click="tryClose" class="backdrop"></div>
-    <dialog open v-if="show">
-      <header>
-        <slot name="header">
-          <h2>{{ title }}</h2>                                  <!-- can change the header -->
-        </slot>
-      </header>
-      <section>
-        <slot></slot>                                           <!-- can change main section -->
-      </section>
-      <menu v-if="!fixed">                                      <!-- when set to fixed, cant be closed via button -->
-        <slot name="actions">   
-          <base-button @click="tryClose">Close</base-button>    <!-- can change actions -->
-        </slot>
-      </menu>
-    </dialog>
+    <transition name="dialog">
+      <dialog open v-if="show">
+        <header>
+          <slot name="header">
+            <h2>{{ title }}</h2>                                  <!-- can change the header -->
+          </slot>
+        </header>
+        <section>
+          <slot></slot>                                           <!-- can change main section -->
+        </section>
+        <menu v-if="!fixed">                                      <!-- when set to fixed, cant be closed via button -->
+          <slot name="actions">   
+            <base-button @click="tryClose">Close</base-button>    <!-- can change actions -->
+          </slot>
+        </menu>
+      </dialog>
+    </transition>
   </teleport>
 </template>
 
@@ -94,6 +96,26 @@ menu {
   display: flex;
   justify-content: flex-end;
   margin: 0;
+}
+
+.dialog-enter-from,
+.dialog-leave-to {
+  opacity: 0;
+  transform: scale(0.8);
+}
+
+.dialog-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.dialog-leave-active {
+  transition: all 0.3s ease-in;
+}
+
+.dialog-enter-to,
+.dialog-leave-from {
+  opacity: 1;
+  transform: scale(1);
 }
 
 @media (min-width: 768px) {
--- src/pages/requests/RequestsRecieved.vue
+++ src/pages/requests/RequestsRecieved.vue
@@ -1,23 +1,24 @@
 <template>
-    <!-- the '!!' ensures 'error' is converted from string to truthy boolean -->
-    <base-dialog :show="!!error" title="An error occurred!" @close="handleError">   
-        <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-        <base-card>
-            <header>
-                <h2>Requests Receieved</h2>
-            </header>
-            <base-spinner v-if="isLoading"></base-spinner>
-            <ul v-else-if="hasRequests && !isLoading">
-                <request-item v-for="request in receivedRequests" 
-                    :key="request.id"
-                    :email="request.userEmail"
-                    :message="request.message"></request-item>
-            </ul>
-            <h3 v-else>You haven't received any requests yet!</h3>
-        </base-card>
-    </section>
+    <div>
+        <base-dialog :show="!!error" title="An error occurred!" @close="handleError">   
+            <p>{{ error }}</p>
+        </base-dialog>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Requests Receieved</h2>
+                </header>
+                <base-spinner v-if="isLoading"></base-spinner>
+                <ul v-else-if="hasRequests && !isLoading">
+                    <request-item v-for="request in receivedRequests" 
+                        :key="request.id"
+                        :email="request.userEmail"
+                        :message="request.message"></request-item>
+                </ul>
+                <h3 v-else>You haven't received any requests yet!</h3>
+            </base-card>
+        </section>
+    </div>
 </template>
 
 
